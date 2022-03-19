9ebcbf8780f28487c8dce62a9579fe714712b5ef
nguyentrihuyen1810 <93263209+nguyentrihuyen1810@users.noreply.github.com>
2022-03-19 11:36:08 +0700

    push bài 1, 2

---
+++ b1. CSDL_quan_he/bai_tap/Xay_dung_CSDL_quan_ly_sinh_vien.sql
@@ -0,0 +1,20 @@
+CREATE TABLE student_manager.student (
+	ID INT PRIMARY KEY,
+    NAME VARCHAR(50),
+    AGE INT NULL,
+    COUNTRY VARCHAR(50) NULL
+);
+
+CREATE TABLE student_manager.class(
+	ID INT PRIMARY KEY,
+    NAME VARCHAR(25)
+);
+
+CREATE TABLE student_manager.teacher(
+	ID INT PRIMARY KEY,
+    NAME VARCHAR(50),
+    AGE INT NULL,
+    COUNTRY VARCHAR(50) NULL
+);
+    
+
